8431e3c328e5eba2306572ef4a71b905489adbdb
Nickolai Whitlow <ananasviolet@fintmcwells.com>
2019-05-17 15:17:17 -0700

    Added protection to ft_strsum

--- ft_strsum.c
+++ ft_strsum.c
@@ -6,7 +6,7 @@
 /*   By: nwhitlow <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 18:18:16 by nwhitlow          #+#    #+#             */
-/*   Updated: 2019/05/10 18:22:36 by nwhitlow         ###   ########.fr       */
+/*   Updated: 2019/05/17 15:16:53 by nwhitlow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strsum(const char *s1, const char *s2)
 {
 	char	*sum;
 
+	if (!s1 || !s2)
+		return (NULL);
 	sum = ft_strnew((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char));
 	if (sum == NULL)
 		return (NULL);
